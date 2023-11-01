8f2e9df266c0365619a1db1852cfa2c5b3248d86
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-11-01 17:34:20 +0700

    xong cart.html

--- assets/js/product-detail.js
+++ assets/js/product-detail.js
@@ -64,35 +64,3 @@ if (dots) {
     })
   })
 }
-
-
-
-//!button increase, decrease cart
-const buttons = document.querySelectorAll('.btn')
-if (buttons && buttons.length > 0) {
-  buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const input = button.parentElement.parentElement.querySelector('input')
-      const inputValue = parseInt(input.value)
-      if (button.classList.contains('increase')) {
-        input.value = inputValue + 1
-      } else if (button.classList.contains('decrease')) {
-        if (inputValue > 1) {
-          input.value = inputValue - 1
-        }
-      }
-    })
-  })
-}
-
-// ! validate quantity
-const buttonSubmit = document.querySelector('.button-add-cart')
-if (buttonSubmit) {
-  buttonSubmit.addEventListener('click', (e) => {
-    const input = document.querySelector('.quantity input')
-    if (input.value <= 0 || !input.value) {
-      e.preventDefault()
-      alert('Vui lòng nhập lại số lượng sản phẩm')
-    }
-  })
-}
