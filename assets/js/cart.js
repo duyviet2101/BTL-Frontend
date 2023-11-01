494da658fadbd9c3278487e79658ff9a9012a566
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-11-01 16:45:39 +0700

    table cart

---
+++ assets/js/cart.js
@@ -0,0 +1,11 @@
+import getWall from './layout/wall.js'
+import getTopNav from './layout/topNav.js'
+
+getTopNav('')
+
+getWall({
+  title: 'Giỏ hàng',
+  buttons:false,
+  subContent:'',
+  img:false
+})
