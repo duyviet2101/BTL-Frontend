ebaac497bfeb9c1099d21e41996f8b995b071670
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-11-02 13:51:42 +0700

    trang checkout.html

---
+++ assets/js/checkout.js
@@ -0,0 +1,11 @@
+import getWall from './layout/wall.js'
+import getTopNav from './layout/topNav.js'
+
+getTopNav('')
+
+getWall({
+  title: 'Thanh toán',
+  buttons:false,
+  subContent:'',
+  img:false
+})
